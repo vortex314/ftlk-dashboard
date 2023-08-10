fa83d4870ab98b98e2314a1549c61a8f95788cb0
lieven <lieven.merckx@gmail.com>
2023-08-10 16:13:41 +0200

    added func again

--- src/pubsub/mod.rs
+++ src/pubsub/mod.rs
@@ -12,3 +12,63 @@ pub enum PubSubCmd {
     Unsubscribe{ pattern: String },
     Publish{ topic: String, message: String },
 }
+
+
+pub fn split_underscore(str: &String) -> (Option<&str>, Option<&str>) {
+    let mut it = str.split("_");
+    (it.next(), it.next())
+}
+
+fn get_array_of_2(object: &Value, key: &str) -> Option<(i64, i64)> {
+    info!(
+        " get array of 2 for '{}' in {:?}",
+        key,
+        object[key].as_sequence()
+    );
+    let field1 = object[key]
+        .as_sequence()
+        .and_then(|seq| Some(seq[0].clone()))
+        .and_then(|v| v.as_i64());
+
+    let field2 = object[key]
+        .as_sequence()
+        .and_then(|seq| Some(seq[1].clone()))
+        .and_then(|v| v.as_i64());
+
+    field1
+        .and(field2)
+        .and(Some((field1.unwrap(), field2.unwrap())))
+}
+
+pub fn get_size(object: &Value) -> Option<(i32, i32)> {
+    let pos = get_array_of_2(object, "size");
+    pos.and_then(|(v1, v2)| {
+        let f1 = i32::try_from(v1);
+        let f2 = i32::try_from(v2);
+        if f1.is_ok() && f2.is_ok() {
+            Some((f1.unwrap(), f2.unwrap()))
+        } else {
+            None
+        }
+    })
+}
+
+pub fn get_pos(object: &Value) -> Option<(usize, usize)> {
+    let pos = get_array_of_2(object, "pos");
+    pos.and_then(|(v1, v2)| {
+        let f1 = usize::try_from(v1);
+        let f2 = usize::try_from(v2);
+        if f1.is_ok() && f2.is_ok() {
+            Some((f1.unwrap(), f2.unwrap()))
+        } else {
+            None
+        }
+    })
+}
+
+pub fn value_string_default(object: &Value, key: &str, default: &str) -> String {
+    object[key]
+        .as_str()
+        .map(String::from)
+        .unwrap_or(String::from(default))
+}
